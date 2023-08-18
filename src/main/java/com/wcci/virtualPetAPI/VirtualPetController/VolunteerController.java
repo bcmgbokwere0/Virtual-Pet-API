5f40bb3c772aed4cdff2d9164a4339da47a4ac23
ayailin <yailinacosta22@gmail.com>
2023-08-17 22:28:56 -0500

    Volunteer controller and service update :)

--- src/main/java/com/wcci/virtualPetAPI/VirtualPetController/VolunteerController.java
+++ src/main/java/com/wcci/virtualPetAPI/VirtualPetController/VolunteerController.java
@@ -1,11 +1,11 @@
 package com.wcci.virtualPetAPI.VirtualPetController;
 
-<<<<<<<HEAD
-
 import java.security.Provider.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,65 +15,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wcci.virtualPetAPI.entity.RoboDog;
 import com.wcci.virtualPetAPI.entity.Volunteer;
-import com.wcci.virtualPetAPI.service.RoboDogService;
-
-@RestController
-@RequestMapping("/volunteer")
-public class VolunteerController {
-
-    @Autowired
-    Volunteer volunteerService;
-
-    @PostMapping("/new")
-    public void addVolunteer(@RequestBody Volunteer Volunteer) {
-        this.volunteerService.createVolunteer(Volunteer);
-    }
-
-    @GetMapping("/findall")
-    public List<Volunteer> findAllVolunteers() {
-        return this.volunteerService.getAllVolunteers();
-    }
-
-    @GetMapping("/{id}")
-    public Volunteer findVolunteerById(@PathVariable String id) {
-        return this.volunteerService.getVolunteerById(id);
-    }
-
-    @PutMapping("/modify/{id}")
-    public Volunteer modifyVolunteer(@PathVariable String id, @RequestBody Volunteer updateVolunteer) {
-        return this.volunteerService.updateVolunteer(id, updateVolunteer);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public void removeVolunteer(@PathVariable String id) {
-        this.volunteerService.deleteVolunteer(id);
-    }
-}
 
 import com.wcci.virtualPetAPI.repository.VolunteerRepository;
+import com.wcci.virtualPetAPI.service.VolunteerService;
 
 @RestController
-@RequestMapping("/api/volunteers")
+@RequestMapping("/volunteers")
 public class VolunteerController {
 
     @Autowired
     private VolunteerRepository volunteerRepository;
 
-    @GetMapping("/getall")
-    public List<Volunteer> getAllVolunteers() {
-        return volunteerRepository.findAll();
-    }
-
-    @GetMapping("/{name}")
-    public Volunteer getVolunteerByName(@PathVariable String name) {
-        return volunteerRepository.findByName(name);
-    }
-
+    // New volunteer
     @PostMapping("/new")
     public Volunteer addVolunteer(@RequestBody Volunteer volunteer) {
         return volunteerRepository.save(volunteer);
     }
 
-}>>>>>>>dfdc041d08ccb633e4a8e52f836979ef00a22c43
+    // Find volunteers
+    @GetMapping("/findall")
+    public List<Volunteer> getAllVolunteers() {
+        return volunteerRepository.findAll();
+    }
+
+    // Find volunteer by name/ id?
+    @GetMapping("/{name}")
+    public Volunteer getVolunteerByName(@PathVariable String name) {
+        return volunteerRepository.findByName(name);
+    }
+
+    // Update existing volunteer
+    @PutMapping("/modify/{id}")
+    public Volunteer modifyVolunteer(@PathVariable String id, @RequestBody Volunteer updateVolunteer) {
+        return this.volunteerRepository.updateVoluteer(id, updateVolunteer);
+    }
+
+    // Delete volunteer
+    @DeleteMapping("/delete/{id}")
+    public void removeVolunteer(@PathVariable String id) {
+        this.volunteerRepository.deleteById(id);
+    }
+
+}
--- src/main/java/com/wcci/virtualPetAPI/service/VolunteerService.java
+++ src/main/java/com/wcci/virtualPetAPI/service/VolunteerService.java
@@ -1,7 +1,5 @@
 package com.wcci.virtualPetAPI.service;
 
-<<<<<<<HEAD
-
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,66 +14,37 @@ public class VolunteerService {
     @Autowired
     private VolunteerRepository volunteerRepository;
 
-    <<<<<<<HEAD
-
+    // create volunteers
     public void createVolunteer(Volunteer volunteer) {
-        this.volunteerRepository.save(null);
+        this.volunteerRepository.save(volunteer);
     }
 
-    =======
-    // Fetch all volunteers
-    >>>>>>>dfdc041d08ccb633e4a8e52f836979ef00a22c43
-
+    // Find all volunteers
     public List<Volunteer> getAllVolunteers() {
         return volunteerRepository.findAll();
     }
 
-    <<<<<<<HEAD
-
+    // Find volunteer by ID
     public Volunteer getVolunteerById(String id) {
-        return null;
+        Optional<Volunteer> optionalVolunteer = volunteerRepository.findById(id);
+        return optionalVolunteer.orElseThrow();
     }
 
-    public Volunteer updateVolunteer(String id, Volunteer Volunteer) {
-        Volunteer existingVolunteer = getVolunteerById(id);
-
-        if (existingVolunteer != null) {
-            existingVolunteer.setName(updateVolunteer.getName());
-            if (existingVolunteer instanceof Volunteer) {
-                VolunteerRepository.save((Volunteer) existingVolunteer);
-            }
-        }
-        return null;
-    }
-
-    public void deleteVolunteerById(String id) {
-        VolunteerRepository.deleteById(id);
-    }
-
-}=======
-
-    // Fetch volunteer by name
-    public Volunteer getVolunteerByName(String name) {
-        return volunteerRepository.findByName(name);
-    }
-
-    // Add a new volunteer
-    public Volunteer addVolunteer(Volunteer volunteer) {
-        // Here, you can add any business logic before saving the volunteer.
-        return volunteerRepository.save(volunteer);
-    }
-
-    // Delete a volunteer by ID
-    public void deleteVolunteer(String id) {
-        volunteerRepository.deleteById(id);
-    }
-
-    // Update a volunteer
+    // Update existing volunteer
     public Volunteer updateVolunteer(Volunteer volunteer) {
-        if(volunteerRepository.existsById(volunteer.getName())) {
+        if (volunteerRepository.existsById(volunteer.getName())) {
             return volunteerRepository.save(volunteer);
         }
-        return null; 
+        return null;
     }
 
-}>>>>>>>dfdc041d08ccb633e4a8e52f836979ef00a22c43
+    // Delete a volunteer
+    public boolean deleteVolunteerById(String id) {
+        if (volunteerRepository.existsById(id))
+            ;
+        {
+            volunteerRepository.deleteById(id);
+            return true;
+        }
+    }
+}
